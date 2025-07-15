2759ed188c3985a7a071d08c7f78c2681e2e534c
Sanju Shree Varshini K K <153351507+SanjuShreeVarshini@users.noreply.github.com>
2025-07-15 22:30:14 +0530

    Add files via upload

---
+++ 1Dgame.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int numbers[5] = {0};  //first we have to initialize it to zero
+    srand(time(0));
+
+    int treasureIndex = rand () % 5;
+    numbers[treasureIndex] = 1;
+
+    c0ut<< "Welcome to treasure hunt game! \n";
+    cout<<"There are 5 positions.one has a hidden number\n";
+
+    int guess,attempts =0;
+    while(true){
+        cout<<"guess the index( 0 to 4):";
+        cin>>guess;
+        attempts++;
+        if (guess <0||guess > 4){
+            cout<<"Invalid index.guess between 0 & 4\n";
+            coontinue;
+        if(numbers[guess] == 1){
+            cout<<"Congratulations! You found the treasure<<attempts<"attempts.\n";
+            break;
+        else{
+        cout<<"Wrong Spot.Try again"}
+        }
+
+        }
+
+    }
+
+    
+}
